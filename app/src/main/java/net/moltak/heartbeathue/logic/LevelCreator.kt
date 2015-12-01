995ff9d65c33fa692a3559c0db6f5f1f2bb3067d
moltak <moltak@gmail.com>
2015-12-01 11:12:10 +0900

    Level 만드는중...

--- app/src/main/java/net/moltak/heartbeathue/logic/LevelCreator.kt
+++ app/src/main/java/net/moltak/heartbeathue/logic/LevelCreator.kt
@@ -6,14 +6,33 @@ import java.util.*
  * Created by engeng on 11/26/15.
  */
 class LevelCreator {
-    val list: MutableList<HueStage> = ArrayList()
+    private val list: MutableList<Hues> = ArrayList()
 
     constructor() {
         // n = color - color * (1/stage^2)
-        list.add(HueStage(0, 0, 0))
+        var i = 0
+        val rand = Random()
+        rand.setSeed(Date().time)
+
+        while (i < 20) {
+            var hues = Array(3, { i -> HueStage(0, 0, 0)})
+
+            val r = rand.nextInt(255)
+            val g = rand.nextInt(255)
+            val b = rand.nextInt(255)
+
+            var j = 0
+            while (j < 3) {
+                hues[j] = HueStage(r, g, b)
+                j ++
+            }
+
+            list.add(Hues(hues))
+            i ++
+        }
     }
 
-    fun getStages(): List<HueStage> {
+    fun getHues(): List<Hues> {
         return list
     }
 }
--- app/src/test/java/net/moltak/heartbeathue/LevelUnitTest.kt
+++ app/src/test/java/net/moltak/heartbeathue/LevelUnitTest.kt
@@ -4,11 +4,13 @@ import net.moltak.heartbeathue.logic.LevelCreator
 import org.junit.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
 
 /**
  * Created by engeng on 11/26/15.
  */
 class LevelUnitTest {
+
     @Test
     @Throws(Exception::class)
     fun levelObjectHasBeenCreated() {
@@ -18,8 +20,15 @@ class LevelUnitTest {
 
     @Test
     @Throws(Exception::class)
-    fun levelObjectHasHad10Stages() {
+    fun levelObjectHasHad20Stages() {
         var levelCreator = LevelCreator()
-        assertEquals(0, levelCreator.getStages().get(0).R)
+        assertEquals(20, levelCreator.getHues().size)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun stagesHaveADifferentColors() {
+        var levelCreator = LevelCreator()
+        assertTrue(levelCreator.getHues().get(0).hues.get(0).R != levelCreator.getHues().get(1).hues.get(1).R)
     }
 }
