e2baba57f64029da95d2466aa5179679107ea5b8
moltak <moltak@gmail.com>
2015-12-01 13:28:17 +0900

    hue library has been added

--- app/src/main/java/net/moltak/heartbeathue/MainActivity.kt
+++ app/src/main/java/net/moltak/heartbeathue/MainActivity.kt
@@ -2,11 +2,13 @@ package net.moltak.heartbeathue
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.philips.lighting.hue.sdk.PHHueSDK
 
 public class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
