2b7a5e1370e63697467997a40b8dedb191182fb6
경호 <moltak@moltak-iMac.local>
2015-12-08 01:27:33 +0900

    에라잉 어려웡

--- app/src/main/java/net/moltak/heartbeathue/logic/color/CieOppositeXyColorCreator.kt
+++ app/src/main/java/net/moltak/heartbeathue/logic/color/CieOppositeXyColorCreator.kt
@@ -1,6 +1,7 @@
 package net.moltak.heartbeathue.logic.color
 
 import net.moltak.heartbeathue.logic.HueStage
+import java.util.*
 
 /**
  * Created by moltak on 15. 12. 8..
@@ -12,7 +13,21 @@ class CieOppositeXyColorCreator : SpecialColorCreator {
             field = value
         }
 
+    val MAX_COLOR = 255
+
     override fun create(stage: Int): Array<HueStage> {
-        return Array(hueCount, { HueStage(0, 0, 0)})
+        val rand = Random()
+        rand.setSeed(Date().time)
+
+        val r = rand.nextInt(MAX_COLOR)
+        val g = rand.nextInt(MAX_COLOR)
+        val b = rand.nextInt(MAX_COLOR)
+
+        val hueStages = Array(hueCount, { HueStage(r, g, b)})
+
+    }
+
+    private fun createMinimumValue(stage: Int): Int {
+        return (MAX_COLOR * (1 / stage.toFloat())).toInt()
     }
 }
