15ea301e27d3c4e567e623812508c637b7e0a57a
moltak <moltak@gmail.com>
2015-12-12 16:29:48 +0900

    .

--- app/src/main/java/net/moltak/heartbeathue/MainActivity.kt
+++ app/src/main/java/net/moltak/heartbeathue/MainActivity.kt
@@ -28,7 +28,7 @@ public class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_stage)
         ButterKnife.bind(this)
 
         hueController = HueController(HueSharedPreferences.getInstance(this), listener, levelCreator)
