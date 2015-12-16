79c8db47d8aa924481a3c9ee2c1e6b8c4e95c016
정경호 <moltak@gmail.com>
2015-12-17 01:17:23 +0900

    Add new stages

--- app/src/main/java/net/moltak/heartbeathue/logic/color/PartialColorBlindnessCreator.kt
+++ app/src/main/java/net/moltak/heartbeathue/logic/color/PartialColorBlindnessCreator.kt
@@ -7,7 +7,7 @@ import net.moltak.heartbeathue.logic.BulbColor
  */
 class PartialColorBlindnessCreator : SpecialColorCreator {
     override val bulbCount: Int = 3
-    override val stageCount: Int = 7
+    override val stageCount: Int = 9
 
     // stage가 1부터 들어옴.
     override fun create(stage: Int): Array<BulbColor> {
@@ -23,34 +23,44 @@ class PartialColorBlindnessCreator : SpecialColorCreator {
                 array[2] = BulbColor(0x0068FE)
             }
             1 -> {
-                array[0] = BulbColor(0xFF0000)
-                array[1] = BulbColor(0x8F9022)
-                array[2] = BulbColor(0xB93B00)
+                array[0] = BulbColor(0x723E3D)
+                array[1] = BulbColor(0xD9191A)
+                array[2] = BulbColor(0xFF0000)
             }
             2 -> {
-                array[0] = BulbColor(0xFF0000)
-                array[1] = BulbColor(0xBA7E0B)
-                array[2] = BulbColor(0xE6210B)
+                array[0] = BulbColor(0x00FF00)
+                array[1] = BulbColor(0x21E423)
+                array[2] = BulbColor(0x77AB76)
             }
             3 -> {
+                array[0] = BulbColor(0x0000FF)
+                array[1] = BulbColor(0x078B8B)
+                array[2] = BulbColor(0x01FE25)
+            }
+            4 -> {
+                array[0] = BulbColor(0x8F9022)
+                array[1] = BulbColor(0xB93B00)
+                array[2] = BulbColor(0xFF0000)
+            }
+            5 -> {
+                array[0] = BulbColor(0xE6210B)
+                array[1] = BulbColor(0xFF0000)
+                array[2] = BulbColor(0xBA7E0B)
+            }
+            6 -> {
                 array[0] = BulbColor(0xFF0000)
                 array[1] = BulbColor(0xA6A536)
                 array[2] = BulbColor(0xD73000)
             }
-            4 -> {
-                array[0] = BulbColor(0xFF0000)
-                array[1] = BulbColor(0xB06609)
+            7 -> {
+                array[0] = BulbColor(0xB06609)
+                array[1] = BulbColor(0xFF0000)
                 array[2] = BulbColor(0xE61C0C)
             }
-            5 -> {
+            8 -> {
                 array[0] = BulbColor(0x00FF00)
                 array[1] = BulbColor(0x4F993A)
                 array[2] = BulbColor(0x18E415)
-            }
-            6 -> {
-                array[0] = BulbColor(0x0000FF)
-                array[1] = BulbColor(0x078B8B)
-                array[2] = BulbColor(0x01FE25)
             }
         }
 
